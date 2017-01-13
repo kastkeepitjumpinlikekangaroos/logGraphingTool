8ffee5ef6632e90e433d2c1ec2a6d078791aca53
ochrist2 <ochrist2@uwo.ca>
2017-01-12 22:09:21 -0500

    Some bug fixes

--- LogGraphs/Ctrl.js
+++ LogGraphs/Ctrl.js
@@ -14,30 +14,31 @@ angular.module('app').controller('Ctrl',function($scope){
       $scope.base = Math.E;
     }
     $scope.a = parseFloat($scope.a);
-    behold = $scope.b;
-    $scope.b = 1/parseFloat($scope.b);
+    $scope.b = parseFloat($scope.b);
     $scope.c = parseFloat($scope.c);
     $scope.d = parseFloat($scope.d);
     $scope.exp = parseFloat($scope.exp);
     $scope.base = parseFloat($scope.base);
     var x = 1;
     var y = 0;
-    x = x*$scope.b + $scope.c;
+    x = x/$scope.b + $scope.c;
     y = y + $scope.d;
     $scope.transformedPointString ="Your transformed point is at (" + x + "," + y + ").";
     y = -$scope.d;
-    y = y/($scope.exp*$scope.a);
+    y = y/($scope.a);
     y = Math.pow($scope.base, y);
-    y = y*$scope.b;
-    y = y+$scope.c;
+    y = y/$scope.b;
     y = Math.pow(y, (1/$scope.exp))
+    y = y+$scope.c;
     $scope.finishedPointString = "Your  x intercept is " + y + ". Your equation of the asymptote is x = " + $scope.c + ".";
     if ($scope.exp%2 == 0){
-    $scope.extraString = "Your other x intercept is x = " + (-y);
-    $scope.b = behold;
+    $scope.extraString = "Your other x intercept is x = " + ($scope.c-xDistance(y, $scope.c));
     }
     else{
       $scope.extraString = "";
     }
+  }
+  function xDistance(x1, x2){
+    return Math.abs(x1-x2);
   }
 })
